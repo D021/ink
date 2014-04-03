93c6e586511df2e7b400eb7418aa586309086ed2
diegoossa <diegoossa@gmail.com>
2014-04-02 23:48:41 -0500

    Se crea el script de parallax
    Se crea el script de parallax

---
+++ Assets/Scripts/Camera/ParallaxController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace com.inkrunner.camera
+{	
+	/// <summary>
+	/// Controls the scrolling of the layers
+	/// </summary>
+	public class ParallaxController : MonoBehaviour {
+		
+		public float Speed = 0;
+		
+		void Update () 
+		{
+			renderer.material.mainTextureOffset = new Vector2 ((Time.time * Speed) % 1, 0);
+		}
+	}
+}
