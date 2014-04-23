77bd06fbf4c40628b4c7a4ba104a27eca98cd31b
tiag <saalvarezva@gmail.com>
2014-04-22 23:03:41 -0500

    Limpieza del CharacterController y del Animator

--- Assets/Santiago/Scripts/CharacterControllerRunner.cs
+++ Assets/Santiago/Scripts/CharacterControllerRunner.cs
@@ -8,30 +8,20 @@ public class CharacterControllerRunner : MonoBehaviour
     float maxSpeed = 10f;				// The fastest the player can travel in the x axis.
     [SerializeField]
     float jumpForce = 400f;			// Amount of force added when the player jumps.	
-
-    [Range(0, 1)]
-    [SerializeField]
-    float crouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
-
-    [SerializeField]
-    bool airControl = false;			// Whether or not a player can steer while jumping;
+	  
     [SerializeField]
     LayerMask whatIsGround;			// A mask determining what is ground to the character
 
     Transform groundCheck;								// A position marking where to check if the player is grounded.
     float groundedRadius = .2f;							// Radius of the overlap circle to determine if grounded
     bool grounded = false;								// Whether or not the player is grounded.
-    Transform ceilingCheck;								// A position marking where to check for ceilings
-    float ceilingRadius = .01f;							// Radius of the overlap circle to determine if the player can stand up
     Animator anim;										// Reference to the player's animator component.
 
-    bool doubleJump = false;
 
     void Awake()
     {
         // Setting up references.
         groundCheck = transform.Find("GroundCheck");
-        ceilingCheck = transform.Find("CeilingCheck");
         anim = GetComponent<Animator>();
     }
 
@@ -45,32 +35,16 @@ public class CharacterControllerRunner : MonoBehaviour
         // Set the vertical animation
         anim.SetFloat("vSpeed", rigidbody2D.velocity.y);
 
-        if (grounded)//Si toco el suelo puedo volver a saltar
-            doubleJump = false;
     }
 
 
-    public void Move(float move, bool crouch, bool jump)
+    public void Move(float move , bool jump)
     {
 
 
-        // If crouching, check to see if the character can stand up
-        if (!crouch && anim.GetBool("Crouch"))
-        {
-            // If the character has a ceiling preventing them from standing up, keep them crouching
-            if (Physics2D.OverlapCircle(ceilingCheck.position, ceilingRadius, whatIsGround))
-                crouch = true;
-        }
-
-        // Set whether or not the character is crouching in the animator
-        anim.SetBool("Crouch", crouch);
-
         //only control the player if grounded or airControl is turned on
-        if (grounded || airControl)
+        if (grounded)
         {
-            // Reduce the speed if crouching by the crouchSpeed multiplier
-            move = (crouch ? move * crouchSpeed : move);
-
             // The Speed animator parameter is set to the absolute value of the horizontal input.
             anim.SetFloat("Speed", Mathf.Abs(move));
 
@@ -88,7 +62,7 @@ public class CharacterControllerRunner : MonoBehaviour
         }
 
         // If the player should jump...
-        if ((grounded || !doubleJump) && jump)
+        if (grounded && jump)
         {
             // Add a vertical force to the player.
             anim.SetBool("Ground", false);
@@ -96,8 +70,7 @@ public class CharacterControllerRunner : MonoBehaviour
             // y no se vea afectada por la velocidad actual
             rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, 0);
             rigidbody2D.AddForce(new Vector2(0f, jumpForce));
-            if (!grounded)
-                doubleJump = true;
+
         }
     }
 
