48115a0f26d4f262d41033176b70b1402a2b7dfb
diegoossa <diegoossa@gmail.com>
2014-05-24 17:20:45 -0500

    Templo y plataformas one way
    Se coloca el templo y se hace un prefab con la lógica para plataformas
    con collider por arriba.

---
+++ Assets/Scripts/Gameplay/OneSideCollider.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneSideCollider : MonoBehaviour
+{
+
+    public BoxCollider2D platform;
+    public bool oneWay = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        platform.enabled = !oneWay;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log("TriggerStay");
+        oneWay = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        oneWay = false;
+    }
+}
