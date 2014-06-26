a97837aa2ea754606268a01e7f52b7c7221c9cc8
tiag <saalvarezva@gmail.com>
2014-06-26 03:29:12 -0500

    End Script, Credits and Levels

---
+++ Assets/ChangeLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLevel : MonoBehaviour {
+	public string level;
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			Application.LoadLevel(level);
+		}
+	}
+}
---
+++ Assets/Credits.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+	public float target;
+	public float time;
+	public string level;
+	private float yVelocity;
+	// Update is called once per frame
+	void Update () {
+
+		float targetY = Mathf.SmoothDamp(transform.position.y, target, ref yVelocity, time);
+		transform.position = new Vector3(transform.position.x,targetY,transform.position.z);
+
+		if(Mathf.Abs(transform.position.y - target) < 0.5f)
+		Application.LoadLevel(level);
+	}
+
+}
