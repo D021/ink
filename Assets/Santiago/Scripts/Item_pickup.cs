55a9d1dd185b8fa7ce81f89dd2220a41646f107a
diegoossa <diegoossa@gmail.com>
2014-06-26 01:47:44 -0500

    Sonidos de Coins

--- Assets/Santiago/Scripts/Item_pickup.cs
+++ Assets/Santiago/Scripts/Item_pickup.cs
@@ -1,41 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Item_pickup : MonoBehaviour {
+public class Item_pickup : MonoBehaviour
+{
 
 
-	public AudioClip pickupClip;		// Sound for pickup.
-	public int itemType;
-	//Usaremos animacion para el item?
-	//private Animator anim;				// Reference to the animator component.
-	// Use this for initialization
-	void Awake () {
-		UILabel percentage = GameObject.Find("InkPercentage").GetComponent<UILabel>();
-		UISlider slider=GameObject.Find("InkBar").GetComponent<UISlider>();
-		percentage.text=slider.value*100+"%";
-		// Setting up the reference.
-		//Si usaramos animacion
-		//anim = transform.root.GetComponent<Animator>();
-	
-	}
-	
+    public AudioClip pickupClip;		// Sound for pickup.
+    public int itemType;
+    //Usaremos animacion para el item?
+    //private Animator anim;				// Reference to the animator component.
+    // Use this for initialization
+    void Awake()
+    {
+        UILabel percentage = GameObject.Find("InkPercentage").GetComponent<UILabel>();
+        UISlider slider = GameObject.Find("InkBar").GetComponent<UISlider>();
+        percentage.text = slider.value * 100 + "%";
+        // Setting up the reference.
+        //Si usaramos animacion
+        //anim = transform.root.GetComponent<Animator>();
 
-	void OnTriggerEnter2D (Collider2D other) {
+    }
 
-		// If the player enters the trigger zone...
-		if(other.tag == "Player")
-		{
-			// ... play the pickup sound effect.
-			//Sonido para coger items...
-			//AudioSource.PlayClipAtPoint(pickupClip, transform.position);
-			
-			// Increase the number of bombs the player has.
-			//Incrementar la cuenta de items del jugador
-			other.GetComponent<PlayerItems>().addItem(itemType);
 
-			// Destroy the item.
-			Destroy(transform.gameObject);
-		}
-	
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        // If the player enters the trigger zone...
+        if (other.tag == "Player")
+        {
+            // ... play the pickup sound effect.
+            //Sonido para coger items...
+            AudioSource.PlayClipAtPoint(pickupClip, transform.position, 0.5f);
+
+            // Increase the number of bombs the player has.
+            //Incrementar la cuenta de items del jugador
+            other.GetComponent<PlayerItems>().addItem(itemType);
+
+            // Destroy the item.
+            Destroy(transform.gameObject);
+        }
+
+    }
 }
